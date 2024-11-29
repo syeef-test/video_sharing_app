c9e789f6c43099a37431fcc8d46744b5465948f3
syeef <syeefislam@yahoo.com>
2024-11-29 20:16:32 +0530

    instaled cors and dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Backend Server is runing on port ${PORT}`);
-});
+app.use(cors());
+
+const PORT = 3000 || process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+async function startServer() {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("mongodb connected");
+
+    app.listen(PORT, () => {
+      console.log("server is runing on port 3000");
+    });
+  } catch (error) {
+    console.log("error in connecting mongodb", error);
+  }
+}
+
+startServer();
