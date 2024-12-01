68e003251e09f4cf97442bc5e1a2341e786c2e2b
syeef <syeefislam@yahoo.com>
2024-12-01 21:55:37 +0530

    user data fetched

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
 function User() {
-  return <div>User</div>;
+  const [user, setUser] = useState([]);
+
+  const getUserDetails = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const userDetailsResponse = await axios.get(
+        `http://127.0.0.1:3000/api/user/getUser`,
+        { headers: { authorization: token } }
+      );
+      if (userDetailsResponse.status === 200) {
+        console.log(userDetailsResponse);
+        setUser(userDetailsResponse.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserDetails();
+  }, []);
+
+  return (
+    <>
+      <div style={{ textAlign: "center", padding: "20px" }}>
+        <h3>User</h3>
+        <br />
+        <ul style={{ padding: "0", margin: "20px auto", width: "80%" }}>
+          {user.length > 0 ? (
+            user.map((item) => (
+              <li
+                style={{
+                  padding: "10px",
+                  margin: "10px 0",
+                  border: "1px solid #ccc",
+                  borderRadius: "5px",
+                }}
+                key={item._id}
+              >
+                {item.email}
+              </li>
+            ))
+          ) : (
+            <li>No User Data Found</li>
+          )}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default User;
